5b72e9d8fe4fb6d5c7924592601d8294a624f56d
gthenard <thenardduviviergaetan@gmail.com>
2024-02-01 16:18:02 +0100

    go for audit

---
+++ tester map/test.js
@@ -0,0 +1,12 @@
+import { Map } from "../utils/map.js";
+
+let background = new Map("../assets/sprite/background/tiles_space.png")
+let game = document.getElementById('game')
+// background.Next();
+// background.Next();
+game.appendChild(background.HTML)
+
+
+// setInterval(() => {
+//     background.Next();
+// }, 100)
